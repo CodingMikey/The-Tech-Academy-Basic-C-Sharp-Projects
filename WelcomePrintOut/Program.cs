4a3845bc4ec499a9720d995303ce2c06e6f9d664
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-02-03 15:46:51 -0800

    Welcome Screen
    Objective: When the program is run, "Welcome to Acme Accounting Systems" is printed to the console.
    
    Below that, “Remember, we’re “accounting” on you!” is printed to the console.

---
+++ WelcomePrintOut/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace WelcomePrintOut
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re “accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
