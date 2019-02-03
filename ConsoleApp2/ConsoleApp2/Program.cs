9be433cc2d632d81eef802da3d6e415677233660
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-02-03 12:19:59 -0800

    Enter your name
    Enter your name

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.Read();
+        }
+    }
+}
