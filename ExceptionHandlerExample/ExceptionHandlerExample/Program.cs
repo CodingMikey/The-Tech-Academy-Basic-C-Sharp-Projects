cb298a31b1b205cc73c82b808d706adcd9e55a83
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-03-05 12:27:47 -0800

    Add files via upload

---
+++ ExceptionHandlerExample/ExceptionHandlerExample/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number to divide each number in the list");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                List<int> list = new List<int>();
+                list.Add(2);
+                list.Add(4);
+                list.Add(10);
+
+                foreach (int numbers in list)
+                {
+                    Console.WriteLine(numbers / number);
+                    Console.ReadLine();
+                }
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number");
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Cannot divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        
+        }
+    }
+}
