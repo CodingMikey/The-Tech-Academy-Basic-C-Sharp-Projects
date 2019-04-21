df5f3e60454038c1b9f43e45b4cc6f7dfc136ea4
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-04-20 22:34:58 -0700

    Custom Earnings Calc
    Custom Earnings Calc

---
+++ EarningsCalculator/EarningsCalculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EarningsCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many miles did you drive?");
+            double miles = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many stops did you have?");
+            double stops = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many inspections did you do?");
+            double inspections = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Meal pay quanity?");
+            double mealPay = Convert.ToInt32(Console.ReadLine());
+
+            double totalPay = (miles * 0.48) + (stops * 18.50) + (inspections * 18.50) + (mealPay * 25);
+
+            Console.WriteLine("You made ${0} today!", totalPay);
+            Console.ReadLine();
+
+        }
+    }
+}
