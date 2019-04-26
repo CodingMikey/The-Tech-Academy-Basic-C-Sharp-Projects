54800e4b25d56515bbca3991b5f1988b024ee7b5
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-04-26 12:14:50 -0700

    Basic Algorithm Exercise
    Basic Algorithm Exercise

---
+++ BasicAlgorithmExercise/BasicAlgorithmExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicAlgorithmExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(1, 2));
+            Console.WriteLine(test(3, 2));
+            Console.WriteLine(test(2, 2));
+            Console.ReadLine();
+        }
+
+        public static int test(int x, int y)
+        {
+            return x == y ? (x + y) * 3 : x + y;
+        }
+    }
+}
